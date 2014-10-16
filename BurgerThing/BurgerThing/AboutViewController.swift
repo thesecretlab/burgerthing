3d9bd531404af0e0fa51137d95d7ec7cc2a09006
Tim Nugent <timrnugent+dev@gmail.com>
2014-10-16 12:02:13 +1100

    Added an about page

---
+++ BurgerThing/BurgerThing/AboutViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AboutViewController.swift
+//  BurgerThing
+//
+//  Created by Timothy Rodney Nugent on 16/10/2014.
+//  Copyright (c) 2014 Timothy Rodney Nugent. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.title = "About"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
